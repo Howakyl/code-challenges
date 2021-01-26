5a0f15abb6c5f1f38775a018785edb9376cd7940
Howakyl <howakyle96@live.com>
2021-01-25 17:41:50 -0800

    convert number to reverse array

--- challenges.js
+++ challenges.js
@@ -1,10 +1,19 @@
 // 1) Descending Order:
 // takes in a number, and orders the digits in descending order
-function descendingOrder(n){
-  //...
+// function descendingOrder(n){
+//   //...
+//   if (n >= 0) {
+//     let newArr = n.toString().split("");
+//     newArr.sort((a,b) => b - a);
+//     return newArr = parseInt(newArr.join(""))
+//   }
+// }
+
+// 2) Convert number to reversed array of digits
+function digitize(n) {
   if (n >= 0) {
-    let newArr = n.toString().split("");
-    newArr.sort((a,b) => b - a);
-    return newArr = parseInt(newArr.join(""))
-  }
-}
+    return parseInt(n.toString().split('').reverse().join(''))
+  };
+};
+
+console.log(digitize(18459))
