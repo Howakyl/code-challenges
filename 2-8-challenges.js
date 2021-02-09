d9583bcc19312c0512b12dfd35b7aa732259cc90
Howakyl96 <howakyl@gmail.com>
2021-02-08 17:07:24 -0800

    break camel case solution

--- 2-8-challenges.js
+++ 2-8-challenges.js
@@ -1,10 +1,16 @@
 function solution (string) {
 
     for (let i = 0; i < string.length; i++) {
-      if (string.charAt(i) == string[i].toUpperCase()) {
-        console.log(`uppercase letter: ${string[i]}`)
+      if (string.charAt(i) === string[i].toUpperCase()) {
+        string = string.slice(0, i) + ` ${string[i]}` + string.slice(i +1, string.length);
+        i++;
       }
     }
+    return string;
 };
 
-solution("camelCasing")
+function breakCamelCase (string) {
+  return (string.replace(/([A-Z])/g, ' $1'))
+}
+console.log(breakCamelCase("camelCasingTest"));
+// console.log(solution("camelCasingTest"))
