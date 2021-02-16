173fc35d31f6e14edc2f01c55d6077290c0a4408
Howakyl96 <howakyl@gmail.com>
2021-02-15 16:30:26 -0800

    mulitplication table

--- 2-15-challenges.js
+++ 2-15-challenges.js
@@ -1,13 +1,23 @@
 // should return a table based on the argument. return value should be ex: [[1,2,3,] , [2,4,6] , [3,6,9]]
 function multiplicationTable (size) {
   let initalArr = [];
-for (let i = 1; i < size + 1; i++) {
-  initalArr.push(i)
-}
-console.log(initalArr)
+  let totalArr = [];
+  for (let i = 1; i < size + 1; i++) {
+    initalArr.push(i)
+  }
+  let num = 1;
+  while (num < size + 1) {
+    let arr = initalArr.map(nums => {
+      return nums * num;
+    })
+    totalArr.push(arr)
+    num++;
+  }
+  console.log(totalArr)
 }
 
-multiplicationTable(4)
+// multiplicationTable(5)
+
 // 1 2 3 4
 // 2 4 6 8
 // 3 6 9 12
