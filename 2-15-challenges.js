b391b0beb2c6e2a7fcd1d4610280c7513fd15a09
Howakyl96 <howakyl@gmail.com>
2021-02-15 16:54:28 -0800

    car savings function

--- 2-15-challenges.js
+++ 2-15-challenges.js
@@ -22,3 +22,26 @@ function multiplicationTable (size) {
 // 2 4 6 8
 // 3 6 9 12
 // 4 8 12 16
+
+
+// this should return how many months it will take to buy the new car, and how much money he will have left over.
+// round down the remaining money
+// the prices of the NEW and OLD cars decrease by 1.5% each month.
+// Also, every two months, depreceation goes up by .5%.
+function nbMonths (startPriceOld, startPriceNew, savingPerMonth, percentLossByMonth) {
+  let months = 0;
+  let savings = 0;
+  while (startPriceNew > startPriceOld + savings) {
+    savings += savingPerMonth;
+    startPriceOld -= (startPriceOld * (percentLossByMonth /100));
+    startPriceNew -= (startPriceNew * (percentLossByMonth /100));
+    months++;
+
+    if (months % 2 === 0) {
+      percentLossByMonth += .5;
+    }
+  }
+  return [months, Math.round(startPriceOld + savings - startPriceNew)]
+}
+
+console.log(nbMonths(2000, 8000, 1000, 1.5));
