f36592b49f59251851b59ec085a39180b3e660d0
Howakyl96 <howakyl@gmail.com>
2021-02-23 11:59:29 -0800

    average() and even() helper functions

--- 2-23.js
+++ 2-23.js
@@ -4,20 +4,35 @@
 
 // .SQUARE() 
 Array.prototype.square = function () {
-  return this.map(nums => Math.pow(nums,2))
-}
+  return this.map(nums => Math.pow(nums,2));
+};
 
 // .cube()
 Array.prototype.cube = function () {
-  return this.map(nums => Math.pow(nums ,3))
-}
+  return this.map(nums => Math.pow(nums ,3));
+};
 
 // .sum()
 Array.prototype.sum = function () {
-  return this.reduce((a,b) => a + b, 0)
+  return this.reduce((a,b) => a + b, 0);
+};
+
+// .average()
+Array.prototype.average = function () {
+  if (this.length > 0) {
+    return this.reduce((a,b) => a + b) / this.length;
+  } 
+  return NaN
+};
+
+// .even()
+Array.prototype.even = function () {
+  return this.filter(num => num % 2 === 0)
 }
 
 const array = [1, 2, 3, 4, 5];
 console.log(array.square())
 console.log(array.cube())
 console.log(array.sum())
+console.log(array.average())
+console.log(array.even())
