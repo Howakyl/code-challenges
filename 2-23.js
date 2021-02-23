afcdd840ebf50eafcc1443b6f71fdf5db049ed15
Howakyl96 <howakyl@gmail.com>
2021-02-23 11:35:31 -0800

    cube() helper function

--- 2-23.js
+++ 2-23.js
@@ -2,8 +2,16 @@
 // Extend the functionality of built-in Array class with the following methods:
 // square() , cube() , average() , sum() , even() , odd()
 
+// .SQUARE() 
 Array.prototype.square = function () {
   return this.map(nums => Math.pow(nums,2))
 }
-const squareArray = [1, 4 , 9, 16, 25];
-console.log(squareArray.square())
+
+// .cube()
+Array.prototype.cube = function () {
+  return this.map(nums => Math.pow(nums ,3))
+}
+
+const array = [1, 2, 3, 4, 5];
+console.log(array.square())
+console.log(array.cube())
