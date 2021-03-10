045625db2158187ded5b3a0fa30b499996fc7624
Howakyl96 <howakyl@gmail.com>
2021-03-10 12:45:15 -0800

    encrypt this! function

--- 3-8.js
+++ 3-8.js
@@ -9,11 +9,13 @@ function primeGap(g, start, end) {
           divisors.push(k)
         }
       }
+      console.log(divisors)
       if (divisors.length === 1) {
         primes.push(divisors[0])
+        // console.log(primes)
       }
     }
-  //   console.log(primes)
+    // console.log(primes)
     
     let answer ;
     for (let j = 0; j < primes.length; j++) {
@@ -27,13 +29,39 @@ function primeGap(g, start, end) {
     }
   }
 
-console.log(primeGap(8, 300, 400));
+// console.log(primeGap(8, 300, 400));
 
+//// Encrypt this! ////
 
-let num = 109
-for (let i = 2; i < num + 1; i++) {
-  if (num % i === 0) {
-    // console.log(i)
-    return
-  }
+function encryptThis (text) {
+  let final = '';
+  text.split(' ').map(word => {
+    const ascii = word.charCodeAt(0).toString()
+    let string = word.split('').slice(1, word.length)
+    const temp = string[0]
+    string[0] = string[string.length - 1]
+    string[string.length - 1] = temp
+    string = string.join('')
+    final += ascii + string + ' '
+  })
+  return final.trim()
+
 }
+
+console.log(encryptThis('A wise old owl lived in an oak'))
+// console.log(encryptThis('hello world'))
+
+// let thing = 'hello world'
+// thing.split(' ').map(word => console.log(word))
+// console.log(thing.split(' '))
+
+let string1 = '65 119esi 111dl 111lw 108devi 105n 97n 111ka'
+let string2 = '65 119esi 111dl 111lw 108dvei 105n 97n 111ka'
+
+let string3 = 'orld'
+string3 = string3.split('')
+const tmp = string3[0]
+string3[0] = string3[string3.length - 1]
+string3[string3.length - 1] = tmp
+
+// console.log(string3)
