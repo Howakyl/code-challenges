0467152fc841f3d7412d3356e01adf03661b7820
Howakyl <howakyle96@live.com>
2021-06-02 22:41:07 -0700

    findMedianArray solution

--- 5-31.js
+++ 5-31.js
@@ -1,13 +1,13 @@
 // 3 longest substring
 
 function longestSubStr2(string) {
-  let answer = '';
+  let answer = "";
   let num = 0;
   let shortString = string;
   for (let i = 1; i < string.length + 1; i++) {
     let isRepeating = false;
     shortString = string.substr(num, i);
-    let sorted = shortString.split('').sort()
+    let sorted = shortString.split("").sort();
     // console.log(shortString)
     for (let j = 0; j < shortString.length; j++) {
       // if (typeof shortString === "string") {
@@ -15,7 +15,7 @@ function longestSubStr2(string) {
       // }
       if (sorted[j] === sorted[j + 1]) {
         isRepeating = true;
-        num ++;
+        num++;
         i = 1;
       }
       if (!isRepeating && shortString === answer) {
@@ -23,13 +23,13 @@ function longestSubStr2(string) {
       }
     }
     if (!isRepeating && shortString.length > answer.length) {
-      answer = string.substr(num, i )
+      answer = string.substr(num, i);
     }
   }
   if (string === " ") {
-    console.log("answer:",1)
+    console.log("answer:", 1);
   }
-  console.log('answer:' ,answer);
+  console.log("answer:", answer);
 }
 // longestSubStr2("pwwkew");
 // longestSubStr2("abcabcbb")
@@ -38,26 +38,30 @@ function longestSubStr2(string) {
 // longestSubStr2("au")
 // longestSubStr2("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
 
-
 // median two sorted arrays
-function findMedianArray (nums1, nums2) {
-  let combined = [...nums1, ...nums2]
-  combined = combined.sort()
+function findMedianArray(nums1, nums2) {
+  let combined = [...nums1, ...nums2];
+  combined = combined.sort((a, b) => a - b);
+  console.log(combined);
+  if (combined.length === 2) {
+    return console.log((combined[0] + combined[1]) / 2);
+  }
 
   for (let i = combined.length; i > 0; i--) {
     if (combined.length === 1) {
-      return console.log(combined[0])
+      return console.log(combined[0]);
     }
-    combined.pop()
-    combined.shift()
+    combined.pop();
+    combined.shift();
     if (combined.length === 2) {
-      return console.log((combined[0] + combined[1]) / 2)
+      return console.log((combined[0] + combined[1]) / 2);
     }
   }
 }
 
-findMedianArray([1,3], [2])
-findMedianArray([1], [])
-findMedianArray([0,0], [0,0])
-findMedianArray([1,2], [3,4])
-findMedianArray([], [1])
+// findMedianArray([1,3], [2])
+// findMedianArray([1], [])
+// findMedianArray([0,0], [0,0])
+// findMedianArray([1,2], [3,4])
+// findMedianArray([], [1])
+findMedianArray([3], [-2, -1]);
